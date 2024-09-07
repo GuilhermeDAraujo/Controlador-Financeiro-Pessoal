b14e69987cf863101ea6c2cff59c5bc94ac765ff
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2024-09-06 22:05:00 -0300

    Projeto iniciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Controlador_Financeiro_Pessoal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
